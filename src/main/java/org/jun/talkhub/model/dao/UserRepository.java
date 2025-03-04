1ae33c20fb7bd0f9f10c8ef1b550f7ff8966bd06
java-enhanced-1 <jchangjun6@gmail.com>
2025-03-04 13:07:48 +0900

    feat : user model add

---
+++ src/main/java/org/jun/talkhub/model/dao/UserRepository.java
@@ -0,0 +1,32 @@
+package org.jun.talkhub.model.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class UserRepository {
+    private boolean create(String id, String password, String nickname, String gender, int birth) {
+        boolean result = false;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn = DriverManager.getConnection("jdbc:mysql://database.cb4ykygcod66.ap-northeast-2.rds.amazonaws.com:3306/talkhub",
+                    "admin", "jang7890!");
+
+            PreparedStatement ps = conn.prepareStatement("insert into users values (?,?,?,?,?,now())");
+            ps.setString(1, id);
+            ps.setString(2, password);
+            ps.setString(3, nickname);
+            ps.setString(4, gender);
+            ps.setInt(5, birth);
+
+            int r = ps.executeUpdate();
+            result = true;
+
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println("UserDAO.create : " + e.toString());
+        }
+        return result;
+    }
+}
